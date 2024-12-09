84b0ef4da8c9c09f4e5b3e3dc8f349eaf65209bf
Gabriel Oyikwu <gabrieloyikwu@yahoo.com>
2024-12-09 13:26:16 -0800

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    base: "/Budget-Tracker",
+    base: "/Budget-Tracker/"
     // Other build configuration options...
   },
 })
