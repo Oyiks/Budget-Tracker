da25fee2b679fde75d548f0b99e943915e1ceed8
Gabriel Oyikwu <gabrieloyikwu@yahoo.com>
2024-12-09 14:15:11 -0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    base: "/Budget-Tracker/"
+    base: "/Budget-Tracker/",
     // Other build configuration options...
-  },
+
 })
