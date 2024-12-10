8b9ce01b4f6c7df12cd494613e7a757ae9c01417
Gabriel Oyikwu <gabrieloyikwu@yahoo.com>
2024-12-10 14:14:01 -0800

    another commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <BrowserRouter basename="/app">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
     </Provider>
